1327be95d243a0360a94e186249d43b0a05aad97
TurtleWolfe <7327259+TurtleWolfe@users.noreply.github.com>
2021-06-16 07:12:34 -0400

    Section 07 Forms

--- app/components/AppPicker.tsx
+++ app/components/AppPicker.tsx
@@ -63,9 +63,11 @@ const AppPicker: React.FC<AppPickerProps> = ({
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>
-            {selectedItem ? selectedItem.label : placeholder}
-          </AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem.label}</AppText>
+          ) : (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -110,6 +112,10 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
+  },
+  placeholder: {
+    color: defaultStyles.palette.mediumGrey,
+    flex: 1,
   },
   text: {
     flex: 1,
